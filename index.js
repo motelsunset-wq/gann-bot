df8e6aa445e4c902159b2f83fd9b0563f6655d14
motelsunset-wq <motelsunset@gmail.com>
2025-08-02 21:20:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 bot.on("message", (msg) => {
-  bot.sendMessage(msg.chat.id, "Бот работает! ✅");
+  bot.sendMessage(msg.chat.id, "Бот работает!");
 });
